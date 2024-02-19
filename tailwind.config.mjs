691f12f00b5527b27a26d8f65f655a2827a0def3
Victor Colina <victorcolina@G500606MAC272.local>
2024-02-19 20:30:43 -0300

    * feature: Navbar implemented
    * feature: Hero component implemented

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,29 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			animation: {
+				'infinite-scroll': 'infinite-scroll 25s linear infinite'
+			},
+			backgroundColor: {
+				'peach': '#FEF1EE',
+				'white': '#FFFFFF',
+			},
+			colors: {
+				'orange': '#E35738',
+				'blue-deep': '#4B5594'
+			},
+			fontFamily: {
+				'caveat-bush': ['Caveat Brush', 'sans-serif'],
+				'lato': ['Lato', 'sans-serif']
+			},
+			keyframes: {
+				'infinite-scroll': {
+					from: { transform: 'translateX(0)' },
+					to: { transform: 'translateX(-100%)' },
+				}
+			}
+		},
 	},
 	plugins: [],
 }
