c341f0710f5beb1c5ef216713adbcc80986480d3
Victor Colina <victorcolina@G500606MAC272.local>
2024-03-04 17:55:42 -0300

    * feature: landing changed

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,25 +4,37 @@ export default {
 	theme: {
 		extend: {
 			animation: {
-				'infinite-scroll': 'infinite-scroll 25s linear infinite'
+				'infinite-scroll': 'infinite-scroll 25s linear infinite',
+				'heart-animation': 'heart-animation 0.6s ease'
 			},
 			backgroundColor: {
 				'peach': '#FEF1EE',
 				'white': '#FFFFFF',
+				'white-bg': '#F3F0ED'
 			},
 			colors: {
 				'orange': '#E35738',
 				'blue-deep': '#4B5594',
-				'orange-light': '#EECEC7'
+				'orange-light': '#EECEC7',
+				'white-text': '#F5F0ED',
+				'gray': '#323231',
+				'blue': '#6167FF',
+				'blue-light': '#9DA4F0'
 			},
 			fontFamily: {
 				'caveat-bush': ['Caveat Brush', 'sans-serif'],
-				'lato': ['Lato', 'sans-serif']
+				'lato': ['Lato', 'sans-serif'],
+				'grafie': ['Grafie', 'sans-serif']
 			},
 			keyframes: {
 				'infinite-scroll': {
 					from: { transform: 'translateX(0)' },
 					to: { transform: 'translateX(-100%)' },
+				},
+				'heart-animation': {
+					'0%': { transform: 'rotate(0deg)' },
+					'50%': { transform: 'rotate(20deg)' },
+					'100%': { transform: 'rotate(0deg)' }
 				}
 			}
 		},
